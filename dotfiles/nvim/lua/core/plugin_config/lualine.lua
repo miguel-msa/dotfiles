79c80ac56bb4dab0a5f08a655ed227d6c79bbaa8
miguel-msa <71232479+miguel-msa@users.noreply.github.com>
2023-08-30 00:33:35 +0100

    dev into main => working custom nvim config (#7)
    * refactor: struct of plugins/opts in init.lua for lazy pkg manager
    
    * feat: working custom nvim config
    
    * chore: some struct changes

---
+++ dotfiles/nvim/lua/core/plugin_config/lualine.lua
@@ -0,0 +1,8 @@
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'terafox', -- check if this naming is enough
+    component_separators = '|',
+    section_separators = '',
+  }
+}
